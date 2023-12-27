ce9e3bf87c70cb0b48f5ec69e9c532ceb21293c1
argonsys <kenon12552@icloud.com>
2023-12-27 19:16:01 +0900

    新規登録、ログイン、ログアウトView作成

--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.views import(
+    LoginView as BaseLoginView,
+    LogoutView as BaseLogoutView
+)
+from django.views.generic import CreateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import User
+from .forms import SignupForm, LoginForm
+
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = "users/signup.html"
+    success_url = reverse_lazy("top")
+
+
+class LoginView(BaseLoginView):
+    model = User
+    form_class = LoginForm
+    template_name = "users/login.html"
+    success_url = reverse_lazy("top")
+
+
+class LogoutView(BaseLogoutView):
+    model = User
+    success_url = reverse_lazy("top")
