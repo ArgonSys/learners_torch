01d0fa667a94935199ef55349bfef7b5944c8a60
argonsys <kenon12552@icloud.com>
2023-12-27 19:51:24 +0900

    仮のUserForms

---
+++ users/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .models import User
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email",)
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
