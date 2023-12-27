7a8faedd51ab3c23ad7a95a1210ee063ed666f18
argonsys <kenon12552@icloud.com>
2023-12-27 18:22:24 +0900

    Userモデルの作成

--- users/models.py
+++ users/models.py
@@ -1,6 +1,79 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.apps import apps
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    UserManager as BaseUserManager
+)
+from django.contrib.auth.hashers import make_password
+
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
-class User(AbstractUser):
-    pass
+class UserManager(BaseUserManager):
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+
+        email = self.normalize_email(email)
+        if not email:
+            raise ValueError("The given email must be set")
+
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        username = GlobalUserModel.normalize_name(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=False,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+    )
+
+    email = models.EmailField(_("email address"), unique=True)
+
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+        db_table = "users"
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
