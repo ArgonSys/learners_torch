be8a82b0b083827bf29e6d4ba4528f772f7fde9a
argonsys <kenon12552@icloud.com>
2023-12-27 19:51:03 +0900

    仮のtopページ

--- learners_torch/urls.py
+++ learners_torch/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.http import HttpResponse
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", lambda request: HttpResponse(b"Hello"), name="top"),
     path("users/", include("users.urls")),
 ]
