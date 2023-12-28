0e7f66560a431d88b92c16c759f781d611bd159a
argonsys <kenon12552@icloud.com>
2023-12-28 21:13:31 +0900

    各PlansViews作成、ルーティング

--- plans/urls.py
+++ plans/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from .views import PlansIndexView
-
+from .views import(
+    PlansShowView,
+    PlansIndexView,
+    PlansCreateView,
+    PlansUpdateView,
+    PlansDeleteView,
+)
 
 app_name = "plans"
 
 urlpatterns = [
+    path("<int:plan_pk>", PlansShowView.as_view(), name="show"),
     path("", PlansIndexView.as_view(), name="index"),
+    path("new", PlansCreateView.as_view(), name="new"),
+    path("<int:plan_pk>/edit", PlansUpdateView.as_view(), name="edit"),
+    path("<int:plan_pk>/delete", PlansDeleteView.as_view(), name="delete"),
 ]
--- plans/views.py
+++ plans/views.py
@@ -1,8 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseForbidden
 
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Plan
+from .forms import PlanForm
 
 
-class PlansIndexView(View):
+class PlansShowView(LoginRequiredMixin, View):
+    def get(self, request, plan_pk):
+        plan = get_object_or_404(Plan, pk=plan_pk)
+        if request.user != plan.owner and request.user not in plan.permittees:
+            return HttpResponseForbidden("このプランを閲覧することは禁止されています。")
+        context = {"plan": plan}
+        return render(request, "plans/show.html", context)
+
+
+class PlansIndexView(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, "plans/index.html")
+        plans = request.user.plan_set.prefetch_related("owner")
+        context = {"plans": plans}
+        return render(request, "plans/index.html", context)
+
+
+class PlansCreateView(LoginRequiredMixin, View):
+    def get(self, request):
+        form = PlanForm()
+        context = {"form": form}
+        return render(request, "plans/new.html", context)
+
+    def post(self, request):
+        form = PlanForm(request.POST)
+        if form.is_valid:
+            plan = form.save(commit=False)
+            plan.owner = request.user
+            plan.save()
+            return redirect("top")
+        context = {"form": form}
+        return render(request, "plans/new.html", context)
+
+
+class PlansUpdateView(LoginRequiredMixin, View):
+    def get(self, request, plan_pk):
+        plan = get_object_or_404(Plan, pk=plan_pk)
+        if request.user != plan.owner and request.user not in plan.permittees:
+            return HttpResponseForbidden("このプランを閲覧することは禁止されています。")
+
+        form = PlanForm(instance=plan)
+        context = {"form": form}
+        return render(request, "plans/edit.html", context)
+
+    def post(self, request, plan_pk):
+        plan = get_object_or_404(Plan, pk=plan_pk)
+        if request.user != plan.owner and request.user not in plan.permittees:
+            return HttpResponseForbidden("このプランを閲覧することは禁止されています。")
+
+        form = PlanForm(request.POST, instance=plan)
+        if form.is_valid:
+            plan = form.save(commit=False)
+            plan.owner = request.user
+            plan.save()
+            return redirect("plans:show", plan_pk=plan.pk)
+        context = {"form": form}
+        return render(request, "plans/edit.html", context)
+
+
+class PlansDeleteView(LoginRequiredMixin, View):
+    def post(self, request, plan_pk):
+        plan = get_object_or_404(Plan, pk=plan_pk)
+        if request.user != plan.owner:
+            return HttpResponseForbidden("このプランを削除することは禁止されています。")
+        plan.delete()
+        return redirect("top")
