efd66b72f48fb60be9151eb120f3789efb20dfe8
argonsys <kenon12552@icloud.com>
2024-01-01 15:10:28 +0900

    編集、削除、ステージ作成へのリンク

--- stages/urls.py
+++ stages/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import StageCreateView, StageUpdateView
+from .views import StageCreateView, StageUpdateView, StageDeleteView
 
+
+app_name = "stages"
 
 urlpatterns = [
     path("<int:plan_pk>/new", StageCreateView.as_view(), name="new"),
     path("<int:plan_pk>/<int:stage_pk>/edit", StageUpdateView.as_view(), name="edit"),
+    path("<int:plan_pk>/<int:stage_pk>/delete", StageDeleteView.as_view(), name="delete"),
 ]
--- stages/views.py
+++ stages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseForbidden
 from django.views import View
 
 from .models import Stage
@@ -28,8 +29,30 @@ class StageCreateView(View):
 
 
 class StageUpdateView(View):
-    def get(self, request, stage_pk):
+    def get(self, request, plan_pk, stage_pk):
         stage = get_object_or_404(Stage, pk=stage_pk)
         form = StageForm(instance=stage)
-        context = {"form": form, "stage_pk": stage_pk}
+        context = {"form": form, "plan_pk": plan_pk, "stage_pk": stage_pk}
         return render(request, "stages/edit.html", context)
+
+    def post(self, request, plan_pk, stage_pk):
+        stage = get_object_or_404(Stage, pk=stage_pk)
+        form = StageForm(request.POST, instance=stage)
+        if form.is_valid:
+            stage = form.save(commit=False)
+            stage.plan = get_object_or_404(Plan, pk=plan_pk)
+            stage.order = Stage.objects.count() + 1
+            stage.save()
+            return redirect("plans:show", plan_pk=plan_pk)
+        context = {"form": form}
+        return render(request, "stages/new.html", context)
+
+
+class StageDeleteView(View):
+    def post(self, request, plan_pk, stage_pk):
+        plan = get_object_or_404(Plan, pk=plan_pk)
+        if request.user != plan.owner:
+            return HttpResponseForbidden("このステージを削除することは禁止されています。")
+        stage = get_object_or_404(Stage, pk=stage_pk)
+        stage.delete()
+        return redirect("plans:show", plan_pk=plan_pk)
