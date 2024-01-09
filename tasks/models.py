ab8c664b6949260ad334561ef0eb09468082ece5
argonsys <kenon12552@icloud.com>
2024-01-09 12:28:33 +0900

    Taskモデル作成

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(_("name"), max_length=50)
+    description = models.TextField(_("description"), null=True, blank=True)
+    # すべての task を pending stage に移動させてから stage は削除される
+    stage = models.ForeignKey(
+        "stages.Stage", verbose_name=_("stage"), on_delete=models.CASCADE
+    )
+    order = models.IntegerField(_("order"))
+    is_done = models.BooleanField(_("is done"), default=False)
+    date_created = models.DateTimeField(_("created at"), auto_now_add=True)
+    date_updated = models.DateTimeField(_("updated at"), auto_now=True)
+
+    class Meta:
+        db_table = "tasks"
