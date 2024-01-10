d651ca1b62d88b241956a88d4d79224ea910bc86
argonsys <kenon12552@icloud.com>
2024-01-10 18:20:19 +0900

    ドラッギング中もスクロールできるように

--- static/javascripts/horizontal_scroll.js
+++ static/javascripts/horizontal_scroll.js
@@ -1,7 +1,7 @@
 function horizontal_scroll() {
   const scrollArea = document.querySelector(".horizontal-scroll");
   const PX_PER_SCROLL = 100;
-  scrollArea.addEventListener("wheel", (event) => {
+  scrollArea.parentNode.addEventListener("wheel", (event) => {
     // 縦スクロールの抑制
     event.preventDefault();
     scrollArea.scrollLeft += Math.sign(event.deltaY) * PX_PER_SCROLL;
