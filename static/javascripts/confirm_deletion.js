89cf044fabead1df9814a5c2961e2574177b957e
argonsys <kenon12552@icloud.com>
2024-01-15 00:30:32 +0900

    confirm_deletionの実装

---
+++ static/javascripts/confirm_deletion.js
@@ -0,0 +1,19 @@
+function confirmDeletion() {
+  const deleteBtns = document.querySelectorAll(".delete-btn");
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener("click", clickDeleteBtn);
+  });
+}
+
+function clickDeleteBtn (event) {
+  event.preventDefault();
+  const deleteForm = this.closest("form.delete");
+  const contentType = deleteForm.getAttribute("delete-group");
+  const contentName = deleteForm.getAttribute("delete-name");
+  const msg = `本当に削除しますか？\n${contentType}: ${contentName}`
+  if(!confirm(msg)) return null;
+  this.removeEventListener("click", clickDeleteBtn);
+  this.click();
+}
+
+window.addEventListener("load", confirmDeletion);
