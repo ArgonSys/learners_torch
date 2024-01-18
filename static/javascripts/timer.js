7a4af2f310d62626ebf29b5d00da7fcf11c6bf0b
argonsys <kenon12552@icloud.com>
2024-01-18 13:10:34 +0900

    planedTimeとremainTimeの受け渡し

--- static/javascripts/timer.js
+++ static/javascripts/timer.js
@@ -1,25 +1,14 @@
-const VIEW_WIDTH = 100;
-const VIEW_HEIGHT = 100;
-const R = 40;
-const NO_INCLINATION = 0;
+// remainTime, planedTime from measure_time.html
 const UNDER_HALF = 0;
-const OVER_HALF = 1;
 const CLOCK_WISE = 1;
-const REVERSE_CLOCK_WISE = 0;
-
 
 const DERAY_TIME = 100; //ms
-const T = 10000; //ms
-const REMAIN_T = 10000; //ms
-const IS_OVER_HALF_AT_STARTED = (REMAIN_T > T/2)? 1: 0;
-// IS_OVER_HALF_AT_STARTED == 1の時に引きすぎる分を補正
-const THETA_AT_STARTED = 2*Math.PI * REMAIN_T/T;
-const ENDX_AT_STARTED = VIEW_WIDTH/2 - (IS_OVER_HALF_AT_STARTED? 1: -1) * R * Math.cos(THETA_AT_STARTED);
-const ENDY_AT_STARTED = VIEW_HEIGHT/2 + (IS_OVER_HALF_AT_STARTED? 1: -1) * R * Math.sin(THETA_AT_STARTED);
 
-const DELTA_THETA = 2 * Math.PI * DERAY_TIME / T;
+let viewW, viewH, r, incl;
 
-let currentTheta = THETA_AT_STARTED;
+let currentTheta = 2*Math.PI * remainTime / planedTime;
+const DELTA_THETA = 2 * Math.PI * DERAY_TIME / planedTime;
+
 let countdownID;
 let startedTime;
 
@@ -27,18 +16,27 @@ let loopCount=0;
 
 
 function timer() {
+  const mainTimerPi = document.querySelector(".timer-main .timer-pi");
   const timerBtn = document.querySelector(".timer-btn");
   const resetBtn = document.querySelector(".resetBtn");
-  const mainTimerPi = document.querySelector(".timer-main .timer-pi");
-
-  setTimerCirclePath(mainTimerPi, THETA_AT_STARTED);
+  [viewW, viewH, r, incl] = getTimerVars(mainTimerPi);
 
   startedTime = Date.now();
   countdownID = setInterval(countdown, DERAY_TIME);
 }
 
 
-function setTimerCirclePath(timerPi, theta, view_width=VIEW_WIDTH, view_height= VIEW_HEIGHT ,r=R, inclination=NO_INCLINATION) {
+function countdown() {
+  const mainTimerPi = document.querySelector(".timer-main .timer-pi");
+  loopCount++;
+  console.log(loopCount, currentTheta);
+  setTimerCirclePath(mainTimerPi, currentTheta);
+  if(currentTheta <= 0) clearInterval(countdownID);
+  currentTheta -= DELTA_THETA;
+}
+
+
+function setTimerCirclePath(timerPi, theta) {
   // 2つの円弧を組み合わせ、正円も対応できるようにする
   const overHalf = timerPi.querySelector(".overhalf-circle");
   const underHalf = timerPi.querySelector(".underhalf-circle");
@@ -47,18 +45,18 @@ function setTimerCirclePath(timerPi, theta, view_width=VIEW_WIDTH, view_height= 
   const overHalfTheta = Math.min(Math.max(3/2*Math.PI - theta, -Math.PI/2), Math.PI/2);
   const underHalfTheta = Math.min(Math.max(1/2*Math.PI - theta, -Math.PI/2), Math.PI/2);
 
-  // overHalf: endX = VIEW_WIDTH/2 - RcosΘ, endY = VIEW_HEIGHT/2 + RsinΘ
-  // underHalf: endX = VIEW_WIDTH/2 + RcosΘ, endY = VIEW_HEIGHT/2 - RsinΘ
-  const [overHalfEndX, overHalfEndY] = [view_width/2 - r*Math.cos(overHalfTheta), view_height/2 + r*Math.sin(overHalfTheta)];
-  const [underHalfEndX, underHalfEndY] = [view_width/2 + r*Math.cos(underHalfTheta), view_height/2 - r*Math.sin(underHalfTheta)];
+  // overHalf: endX = viewW/2 - RcosΘ, endY = viewH/2 + RsinΘ
+  // underHalf: endX = viewW/2 + RcosΘ, endY = viewH/2 - RsinΘ
+  const [overHalfEndX, overHalfEndY] = [viewW/2 - r*Math.cos(overHalfTheta), viewH/2 + r*Math.sin(overHalfTheta)];
+  const [underHalfEndX, underHalfEndY] = [viewW/2 + r*Math.cos(underHalfTheta), viewH/2 - r*Math.sin(underHalfTheta)];
 
   // d = M startX startY A R R inclination isOverHalf isClockWise endX endY
-  const overHalfD = ["M", `${view_width/2}`, `${view_height/2 + r}`,
-                     "A", `${r}`, `${r}`, `${inclination}`, `${UNDER_HALF}`,
+  const overHalfD = ["M", `${viewW/2}`, `${viewH/2 + r}`,
+                     "A", `${r}`, `${r}`, `${incl}`, `${UNDER_HALF}`,
                      `${CLOCK_WISE}`, `${overHalfEndX}`, `${overHalfEndY}`];
 
-  const underHalfD = ["M", `${view_width/2}`, `${view_height/2 - r}`,
-                      "A", `${r}`, `${r}`, `${inclination}`, `${UNDER_HALF}`,
+  const underHalfD = ["M", `${viewW/2}`, `${viewH/2 - r}`,
+                      "A", `${r}`, `${r}`, `${incl}`, `${UNDER_HALF}`,
                       `${CLOCK_WISE}`, `${underHalfEndX}`, `${underHalfEndY}`];
 
   overHalf.setAttribute("d", overHalfD.join(" "));
@@ -66,13 +64,14 @@ function setTimerCirclePath(timerPi, theta, view_width=VIEW_WIDTH, view_height= 
 }
 
 
-function countdown() {
-  loopCount++;
-  const mainTimerPi = document.querySelector(".timer-main .timer-pi");
-  console.log(loopCount, currentTheta);
-  setTimerCirclePath(mainTimerPi, currentTheta);
-  if(currentTheta <= 0) clearInterval(countdownID);
-  currentTheta -= DELTA_THETA;
+function getTimerVars(timerPi) {
+  const viewBox = timerPi.getAttribute("viewBox").split(" ");
+  const [viewW, viewH] = [viewBox[2] - viewBox[0], viewBox[3] - viewBox[1]];
+
+  const overhalfD = timerPi.querySelector(".overhalf-circle").getAttribute("d").split(" ");
+  const [r, incl] = [overhalfD[4], overhalfD[6]]
+
+  return [Number(viewW), Number(viewH), Number(r), incl];
 }
 
 
--- time_logs/views.py
+++ time_logs/views.py
@@ -1,10 +1,13 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import View
 
 
 class MeasureTimeView(View):
     def get(self, request):
-        return render(request, "time_logs/measure_time.html")
+        context = {"planedTime": 100000, "remainTime": 100000}
+        return render(request, "time_logs/measure_time.html", context)
 
     def post(self, request):
         return render(request, "time_logs/measure_time.html")
