eacc20022ad1e1b77a97d53dbb07ab9b1a63b0ea
argonsys <kenon12552@icloud.com>
2024-01-18 10:59:57 +0900

    残り時間から初期のendX, endYを設定

--- static/javascripts/timer.js
+++ static/javascripts/timer.js
@@ -8,6 +8,13 @@ const CLOCK_WISE = 1;
 
 const DERAY_TIME = 100; //ms
 const T = 10000; //ms
+const REMAIN_T = 8000; //ms
+const IS_OVER_HALF_AT_STARTED = (REMAIN_T > T/2)? 1: 0;
+// IS_OVER_HALF_AT_STARTED == 1の時に引きすぎる分を補正
+const THETA_AT_STARTED = Math.PI/2 - 2*Math.PI * REMAIN_T/T + IS_OVER_HALF_AT_STARTED * Math.PI
+const ENDX_AT_STARTED = VIEW_WIDTH/2 - (IS_OVER_HALF_AT_STARTED? 1: -1) * R * Math.cos(THETA_AT_STARTED);
+const ENDY_AT_STARTED = VIEW_HEIGHT/2 + (IS_OVER_HALF_AT_STARTED? 1: -1) * R * Math.sin(THETA_AT_STARTED);
+
 const DELTA_THETA = 2 * Math.PI * DERAY_TIME / T;
 let countdownID;
 let currentTime;
@@ -20,20 +27,19 @@ function timer() {
   const mainTimerCircle = document.querySelector(".timer-main .timer-circle");
   const subTimerCircle = document.querySelector(".timer-sub .timer-circle");
 
-  // setTimerCirclePath(mainTimerCircle);
-  // d = M startX startY A R R inclination isOverHalf isClockWise endX endY
-  const d = ["M", `${VIEW_WIDTH/2}`, `${VIEW_HEIGHT/2 - R}`,
-             "A", `${R}`, `${R}`, `${INCLINATION}`, `${OVER_HALF}`,
-             `${CLOCK_WISE}`, `${VIEW_WIDTH/2-0.0001}`, `${VIEW_HEIGHT/2 - R}`]
-  mainTimerCircle.setAttribute("d", d.join(" "));
+  setTimerCirclePath(mainTimerCircle);
 
   currentTime = Date.now()
   countdownID = setInterval(countdown, DERAY_TIME);
 }
 
-// function setTimerCirclePath(circlePath) {
-// }
-
+function setTimerCirclePath(circlePath) {
+  // d = M startX startY A R R inclination isOverHalf isClockWise endX endY
+  const d = ["M", `${VIEW_WIDTH/2}`, `${VIEW_HEIGHT/2 - R}`,
+             "A", `${R}`, `${R}`, `${INCLINATION}`, `${OVER_HALF}`,
+             `${CLOCK_WISE}`, `${ENDX_AT_STARTED-0.00001}`, `${ENDY_AT_STARTED}`];
+  circlePath.setAttribute("d", d.join(" "));
+}
 
 function countdown() {
   loopCount++;
@@ -47,10 +53,10 @@ function countdown() {
   let endX = pathList[9];
   let endY = pathList[10];
 
-  if (isOverHalf === "1") {
-    let theta = Math.atan((endY - VIEW_HEIGHT/2) / (VIEW_WIDTH/2 - endX));
+  let theta = Math.atan((VIEW_HEIGHT/2 - endY) / (endX - VIEW_WIDTH/2));
+  theta += DELTA_THETA;
 
-    theta += DELTA_THETA;
+  if (isOverHalf === "1") {
     [endX, endY] = [VIEW_WIDTH/2 - R * Math.cos(theta), VIEW_HEIGHT/2 + R * Math.sin(theta)]
 
     // thetaが pi/2 を上回ったらisOverHalfを変更し、
@@ -64,15 +70,15 @@ function countdown() {
     }
 
   } else {
-    let theta = Math.atan((VIEW_HEIGHT/2 - endY) / (endX - VIEW_WIDTH/2));
-    theta += DELTA_THETA;
+    [endX, endY] = [VIEW_WIDTH/2 + R * Math.cos(theta), VIEW_HEIGHT/2 - R * Math.sin(theta)]
 
     // thetaが -pi/2 を下回ったらthetaを -pi/2 にする
     if (theta >= Math.PI/2) {
-      // theta = Math.PI/2;
-      clearInterval(countdownID)
+      isOverHalf = 1;
+      theta = -Math.PI/2;
+      [endX, endY] = [VIEW_WIDTH/2 - R * Math.cos(theta) - 0.01, VIEW_HEIGHT/2 + R * Math.sin(theta)]
+      // clearInterval(countdownID)
     }
-    [endX, endY] = [VIEW_WIDTH/2 + R * Math.cos(theta), VIEW_HEIGHT/2 - R * Math.sin(theta)]
 
   }
 
