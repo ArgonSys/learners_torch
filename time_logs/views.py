7c6e97c8f031f850f94f94d2a5815f4ab06160ff
argonsys <kenon12552@icloud.com>
2024-01-22 15:27:16 +0900

    dbからタイマーにplaned_timeが渡せるように

--- time_logs/views.py
+++ time_logs/views.py
@@ -3,11 +3,21 @@ import math
 from django.shortcuts import render
 from django.views import View
 
+from tasks.models import Task
+from stages.models import Stage
+from .models import TimeLog
+
 
 class MeasureTimeView(View):
     def get(self, request):
-        planed_time = 10000
-        remain_time = 8000
+        task = Task.objects.get(pk=57)
+        stage = Stage.objects.get(pk=35)
+        planed_time = int(
+            TimeLog.objects.get(task=task, stage=stage).planed_time.total_seconds()
+            * 1000
+        )
+
+        remain_time = planed_time
 
         context = {"planed_time": planed_time, "remain_time": remain_time}
         return render(request, "time_logs/measure_time.html", context)
