4f0e421420106c80b1b57c9b12a462479b50c1b2
argonsys <kenon12552@icloud.com>
2024-02-20 17:52:41 +0900

    ProfileForm, UserFormにplaceholder追加、iconのinput要素を非表示に

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,9 +1,37 @@
 from django import forms
+from django.utils.translation import gettext as _
 
 from .models import Profile
 
 
 class ProfileForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.fields["icon"].widget.attrs.update(
+            {
+                "class": "hidden",
+            }
+        )
+
+        self.fields["bio"].widget.attrs.update(
+            {
+                "placeholder": _("Bio"),
+            }
+        )
+
+        self.fields["affiliation"].widget.attrs.update(
+            {
+                "placeholder": _("Affiliation"),
+            }
+        )
+
+        self.fields["location"].widget.attrs.update(
+            {
+                "placeholder": _("Location"),
+            }
+        )
 
     class Meta:
         model = Profile
--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from django.utils.translation import gettext as _
@@ -6,6 +7,33 @@ from .models import User
 
 
 class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs.update(
+            {
+                "placeholder": _("User name"),
+            }
+        )
+
+        self.fields["email"].widget.attrs.update(
+            {
+                "placeholder": _("Email address"),
+            }
+        )
+
+        self.fields["password1"].widget.attrs.update(
+            {
+                "placeholder": _("Password"),
+            }
+        )
+
+        self.fields["password2"].widget.attrs.update(
+            {
+                "placeholder": _("Password confirmation"),
+            }
+        )
+
     class Meta:
         model = User
         fields = (
