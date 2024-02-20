66c510222f9ccc75c07a90e2b7bdd997edf55533
argonsys <kenon12552@icloud.com>
2024-02-20 15:41:51 +0900

    profile formの追加

--- users/views.py
+++ users/views.py
@@ -3,10 +3,13 @@ from django.contrib.auth.views import (
     LogoutView as BaseLogoutView,
 )
 from django.views.generic import CreateView
+from django.forms import inlineformset_factory
 from django.urls import reverse_lazy
 
 from .models import User
 from .forms import SignupForm, LoginForm
+
+from profiles.models import Profile
 
 
 class SignupView(CreateView):
@@ -14,6 +17,24 @@ class SignupView(CreateView):
     form_class = SignupForm
     template_name = "users/signup.html"
     success_url = reverse_lazy("top")
+
+    def get_context_data(self, **kwargs):
+        ProfileFormset = inlineformset_factory(
+            User,
+            Profile,
+            fields=(
+                "icon",
+                "bio",
+                "affiliation",
+                "location",
+            ),
+            extra=1,
+            can_delete_extra=False,
+        )
+
+        context = super().get_context_data(**kwargs)
+        context["profile_form"] = ProfileFormset()[0]
+        return context
 
 
 class LoginView(BaseLoginView):
