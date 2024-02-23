f17480bd0d2c7e142acbd11fc642e2a8f5ea157a
argonsys <kenon12552@icloud.com>
2024-02-23 19:44:10 +0900

    ヘッダーのユーザーアイコンの表示

--- learners_torch/urls.py
+++ learners_torch/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 from plans.views import PlansIndexView
 
@@ -31,3 +35,5 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
